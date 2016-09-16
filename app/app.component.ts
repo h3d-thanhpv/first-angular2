f67dfcb884483743813b9ef80f50b7862099502b
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-09-16 08:28:09 +0700

    Demo for ngFor array

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,19 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <h2>My favorite hero is: {{myHero}}</h2>
+    <p>Heroes:</p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{ hero }}
+      </li>
+    </ul>
+  `
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'Tour of Heroes';
+    heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+    myHero = this.heroes[0];
+}
